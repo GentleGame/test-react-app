6519161ef073f226e82da4a8b9300b3432ded6b0
GentleGame <banderes94@gmail.com>
2020-03-12 00:30:53 +0300

    modx configureded

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import DevTools from 'mobx-react-devtools';
+
+class App extends React.Component{
+    render(){
+        return (
+        <div className="App">
+            <header className="App-header">
+                {/* <DevTools /> */}
+                <h1>Hello world!</h1>
+            </header>
+        </div>
+        );
+    }
+  }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
