9d810b442c99f0328208f42e824c0d0599a549e9
GentleGame <banderes94@gmail.com>
2020-03-12 12:17:40 +0300

    store done

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import DevTools from 'mobx-react-devtools';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import store from './store.js';
 
-class App extends React.Component{
+@observer class App extends React.Component{
     render(){
         return (
         <div className="App">
-            <header className="App-header">
-                {/* <DevTools /> */}
-                <h1>Hello world!</h1>
-            </header>
+            <h1>{store.count}</h1>
+            <button onClick={store.increment}>+</button>
+            <button onClick={store.decrement}>-</button>
         </div>
         );
     }
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { observable, action, decorate } from 'mobx';
+
+class Store{
+   
+}
+
+const store = new Store();
+decorate (store, {})
+
+export default store;
