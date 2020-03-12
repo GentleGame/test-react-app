cef270feaff58077d951ea3b5444ff0a97d900b8
GentleGame <banderes94@gmail.com>
2020-03-12 13:24:55 +0300

    antd.css added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import * as serviceWorker from './serviceWorker';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import store from './store.js';
+import 'antd/dist/antd.css';
+
+const style = { background: '#0092ff', padding: '8px 0' };
 
 @observer class App extends React.Component{
     render(){
