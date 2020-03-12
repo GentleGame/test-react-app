cf7604e62a9c5acc2264dd762a2a97cd9939c20b
Alex <boyfun760@gmail.com>
2020-03-12 13:30:10 +0300

    Added App.js, imported App.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
 import store from './store.js';
 import 'antd/dist/antd.css';
+import App from './App.js';
 
-const style = { background: '#0092ff', padding: '8px 0' };
-
-@observer class App extends React.Component{
-    render(){
-        return (
-        <div className="App">
-        </div>
-        );
-    }
-  }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
