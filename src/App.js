808a9363a3d3666d8e114220d1bd6a584204e366
GentleGame <banderes94@gmail.com>
2020-03-21 03:43:48 +0300

    incredible navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Main from './Components/Main';
 
 
 class App extends React.Component{
@@ -9,7 +12,9 @@ class App extends React.Component{
 
     render(){
         return <div>
-
+            <BrowserRouter>
+                <Route exact path='/' component={Main} />
+            </BrowserRouter>
         </div>
     }
 }
