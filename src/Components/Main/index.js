6280f57502504265d13e7d0ab725ce663a2e1741
GentleGame <banderes94@gmail.com>
2020-03-21 16:13:53 +0300

    hotfix fix merge conflict

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import './index.css';
 import Header from './Header/index'
 import Steps from './Steps/index'
-<<<<<<< HEAD
+
 import Regalia from '../Regalia';
-=======
 import LastOffers from './LastOffers/index'
->>>>>>> f4add24d022956064b531e1b5914e3952797b777
 
 import Navigation from './Navigation/index';
 
@@ -19,13 +17,10 @@ export class Main extends Component {
             </div>
                 <Header/>
                 <Steps/>
-<<<<<<< HEAD
                 <div className='container'>
                     <Regalia/>
                 </div>
-=======
                 <LastOffers/>
->>>>>>> f4add24d022956064b531e1b5914e3952797b777
             </div>
 
         );
